b3d21386a6b4ceea15ff4f972044688a338dd26a
Mehmet ARI <mehmetari@MacBook-Pro-van-Mehmet.local>
2021-02-04 21:19:26 +0100

    fonksiyon eklendi

--- eda.py
+++ eda.py
@@ -7,6 +7,13 @@ import matplotlib.pyplot as plt
 #############################################
 
 def grab_col_names(dataframe, cat_th=10, car_th=20):
+    '''
+    Deneme...
+    :param dataframe:
+    :param cat_th:
+    :param car_th:
+    :return:
+    '''
 
     cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
 
